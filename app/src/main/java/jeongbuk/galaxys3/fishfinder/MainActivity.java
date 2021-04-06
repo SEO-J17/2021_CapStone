7bfe3936357b591f59ed3af937990c8c6dee3486
SEO-J17 <qw4643987@naver.com>
2021-04-07 01:55:28 +0900

    Initial commit

---
+++ app/src/main/java/jeongbuk/galaxys3/fishfinder/MainActivity.java
@@ -0,0 +1,14 @@
+package jeongbuk.galaxys3.fishfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
