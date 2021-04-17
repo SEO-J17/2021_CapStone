2348102caab41439bf1af9c1c1696b0fe5319c1e
SEO-J17 <qw4643987@naver.com>
2021-04-18 04:22:15 +0900

    Login Function 100% done!

--- app/src/main/java/jeongbuk/galaxys3/fishfinder/MainActivity.java
+++ app/src/main/java/jeongbuk/galaxys3/fishfinder/MainActivity.java
@@ -2,11 +2,27 @@ package jeongbuk.galaxys3.fishfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,17 +30,20 @@ public class MainActivity extends AppCompatActivity {
     private Button login;
     private EditText id;
     private EditText pw;
+    private CheckBox loginstate;
+    private SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       
 
+        sharedPreferences = getSharedPreferences("UserState", Context.MODE_PRIVATE);
         join = findViewById(R.id.btn_join);
         login = findViewById(R.id.btn_login);
-        id = findViewById(R.id.edit_id);
-        pw = findViewById(R.id.edit_pw);
-
+        id = findViewById(R.id.write_id);
+        pw = findViewById(R.id.write_pw);
+        loginstate = findViewById(R.id.chk_login);
 
         //회원가입 버튼 이벤트
         join.setOnClickListener(new View.OnClickListener() {
@@ -41,11 +60,75 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //패스워드와 아이디를 파라미터로 받아 클래스로 전송하기.
-                Intent intent = new Intent(MainActivity.this, Login_Activity.class);
-                startActivity(intent);
+                String ID = id.getText().toString().trim();
+                String PW = pw.getText().toString().trim();
+                if (TextUtils.isEmpty(ID) || TextUtils.isEmpty(PW)) {
+                    Toast.makeText(MainActivity.this, "아이디 또는 비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                } else {
+                    login(ID, PW);
+                }
             }
         });
 
+        //sharedPreference 자동로그인
+        String loginStatus = sharedPreferences.getString(getResources().getString(R.string.prefLoginstate), "");
+        if (loginStatus.equals("loggedin")) {
+            startActivity(new Intent(MainActivity.this, Choice_Activity.class));
+        }
+        
+    }
+
+    private void login(final String id, final String pw) {
+        final ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setCancelable(false);
+        progressDialog.setIndeterminate(false);
+        progressDialog.setTitle("Registering New Account");
+        progressDialog.show();
+
+        String uRl = "http://58.236.108.52/login.php";
+        StringRequest request = new StringRequest(Request.Method.POST, uRl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response)
+            {
+                if (response.equals("Login Success")) {
+                    progressDialog.dismiss();
+                    Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    if (loginstate.isChecked()) {
+                        editor.putString(getResources().getString(R.string.prefLoginstate), "loggedin");
+                    }
+                    else {
+                        editor.putString(getResources().getString(R.string.prefLoginstate), "loggedout");
+                    }
+                    editor.apply();
+                    Intent intent = new Intent(MainActivity.this, Choice_Activity.class);
+                    startActivity(intent);
+                }else {
+                    progressDialog.dismiss();
+                    Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error)
+            {
+                Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError
+            {
+                HashMap<String, String> param = new HashMap<>();
+                param.put("id", id);
+                param.put("password", pw);
+                return param;
+            }
+        };
+
+
+        request.setRetryPolicy(new DefaultRetryPolicy(30000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        MySingleton.getmInstance(MainActivity.this).addToRequestQueue(request);
 
     }
 }
