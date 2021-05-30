084f44f26a5f47e49af3c968f2b152960443c393
SEO-J17 <qw4643987@naver.com>
2021-05-31 04:42:14 +0900

    Write Board Developing...

--- app/src/main/java/jeongbuk/galaxys3/fishfinder/Write_Activity.java
+++ app/src/main/java/jeongbuk/galaxys3/fishfinder/Write_Activity.java
@@ -1,17 +1,35 @@
 package jeongbuk.galaxys3.fishfinder;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.BinderThread;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
 import java.io.File;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Write_Activity extends AppCompatActivity {
     private Button content_regsiter;
@@ -20,6 +38,7 @@ public class Write_Activity extends AppCompatActivity {
     private EditText content;
     private EditText title;
     private ImageView imgview;
+
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,85 +52,82 @@ public class Write_Activity extends AppCompatActivity {
         imgview = (ImageView)findViewById(R.id.board_view);
 
 
-        gallery.setOnClickListener(new View.OnClickListener() {
+        content_regsiter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                BringPhoto();
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
-                startActivityForResult(intent, 1);
+                String Title = title.getText().toString();
+                String Content = content.getText().toString();
+                RegisterContent(Title, Content);
             }
         });
 
-
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 101);
+            }
+        });
     }
 
+    private void RegisterContent(final String title, final String content) {
+        final ProgressDialog progressDialog = new ProgressDialog(Write_Activity.this);
+        progressDialog.setCancelable(false);
+        progressDialog.setIndeterminate(false);
+        progressDialog.setTitle("등록중 입니다.");
+        progressDialog.show();
+
+        String url = "http://58.236.108.52/login.php";
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+        public void onResponse(String response) {
+            if (response.equals("Successfully Registered")) {
+                startActivity(new Intent(Write_Activity.this, Board_Activity.class));
+                finish();
+            } else {
+                Toast.makeText(Write_Activity.this, response, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }, new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Toast.makeText(Write_Activity.this, error.toString(), Toast.LENGTH_SHORT).show();
+        }
+    }) {
+        protected Map<String, String> getParams() throws AuthFailureError {
+            HashMap<String, String> param = new HashMap<>();
+            param.put("title", title);
+            param.put("id", content);
+
+            return param;
+        }
+    };
+
+        request.setRetryPolicy(new DefaultRetryPolicy(30000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                                                      DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        MySingleton.getmInstance(Write_Activity.this).addToRequestQueue(request);    // // Add a request
+    }
+
+
     @Override
-
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode,resultCode,data);
-        if(resultCode != RESULT_OK)
-            return;
-        switch(requestCode){
-            case PICK_FROM_ALBUM:
-            {
-                // 이후의 처리가 카메라와 같으므로 일단  break없이 진행합니다.
-                // 실제 코드에서는 좀더 합리적인 방법을 선택하시기 바랍니다.
-                mImageCaptureUri = data.getData();
-                Log.d("SmartWheel",mImageCaptureUri.getPath().toString());
-            }
-
-
-            case PICK_FROM_CAMERA:
-            {
-                // 이미지를 가져온 이후의 리사이즈할 이미지 크기를 결정합니다.
-                // 이후에 이미지 크롭 어플리케이션을 호출하게 됩니다.
-                Intent intent = new Intent("com.android.camera.action.CROP");
-                intent.setDataAndType(mImageCaptureUri, "image/*");
-                // CROP할 이미지를 200*200 크기로 저장
-                intent.putExtra("outputX", 200); // CROP한 이미지의 x축 크기
-                intent.putExtra("outputY", 200); // CROP한 이미지의 y축 크기
-                intent.putExtra("aspectX", 1); // CROP 박스의 X축 비율
-                intent.putExtra("aspectY", 1); // CROP 박스의 Y축 비율
-                intent.putExtra("scale", true);
-                intent.putExtra("return-data", true);
-                startActivityForResult(intent, CROP_FROM_iMAGE); // CROP_FROM_CAMERA case문 이동
-                break;
-
-            }
-            case CROP_FROM_iMAGE:
-            {
-                // 크롭이 된 이후의 이미지를 넘겨 받습니다.
-                // 이미지뷰에 이미지를 보여준다거나 부가적인 작업 이후에
-                // 임시 파일을 삭제합니다.
-                if(resultCode != RESULT_OK) {
-                    return;
-                }
-                final Bundle extras = data.getExtras();
-                // CROP된 이미지를 저장하기 위한 FILE 경로
-                String filePath = Environment.getExternalStorageDirectory().getAbsolutePath()+
-                        "/SmartWheel/"+System.currentTimeMillis()+".jpg";
-
-
-                if(extras != null){
-                    Bitmap photo = extras.getParcelable("data"); // CROP된 BITMAP
-                    iv_UserPhoto.setImageBitmap(photo); // 레이아웃의 이미지칸에 CROP된 BITMAP을 보여줌
-                    storeCropImage(photo, filePath); // CROP된 이미지를 외부저장소, 앨범에 저장한다.
-                    absoultePath = filePath;
-                    break;
-                }
-
-                // 임시 파일 삭제
-                File f = new File(mImageCaptureUri.getPath());
-                if(f.exists())
-                {
-                    f.delete();
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 101) {
+            if (resultCode == RESULT_OK) {
+                Uri fileUri = data.getData();
+                ContentResolver resolver = getContentResolver();
+                try {
+                    InputStream instream = resolver.openInputStream(fileUri);
+                    Bitmap imgBitmap = BitmapFactory.decodeStream(instream);
+                    imgview.setImageBitmap(imgBitmap);    // 선택한 이미지 이미지뷰에 셋
+                    instream.close();   // 스트림 닫아주기
+                    Toast.makeText(getApplicationContext(), "파일 불러오기 성공", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "파일 불러오기 실패", Toast.LENGTH_SHORT).show();
                 }
             }
         }
-
     }
-
-
 
 }
