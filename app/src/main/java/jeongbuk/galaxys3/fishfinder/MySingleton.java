6779743e9bab4ed43fed385dda92f920377776d1
SEO-J17 <qw4643987@naver.com>
2021-06-04 03:57:23 +0900

    No more use. just backup

--- app/src/main/java/jeongbuk/galaxys3/fishfinder/MySingleton.java
+++ app/src/main/java/jeongbuk/galaxys3/fishfinder/MySingleton.java
@@ -1,44 +1,43 @@
 package jeongbuk.galaxys3.fishfinder;
-
-import android.content.Context;
-
-import com.android.volley.Cache;
-import com.android.volley.Network;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.BasicNetwork;
-import com.android.volley.toolbox.DiskBasedCache;
-import com.android.volley.toolbox.HurlStack;
-import com.android.volley.toolbox.Volley;
-
-public class MySingleton {
-    private static MySingleton mInstance;
-    private RequestQueue mRequestQueue;
-    private Context mCtx;
-
-    public MySingleton(Context mCtx) {
-        this.mCtx = mCtx;
-        mRequestQueue = getmRequestQueue();
-    }
-
-    public RequestQueue getmRequestQueue() {
-        if (mRequestQueue == null) {
-            Cache cache = new DiskBasedCache(mCtx.getCacheDir(), 1024*1024);     //1MB CAP
-            Network network = new BasicNetwork(new HurlStack());    //Set up the network to use HttpURLConnection as the HTTP client.
-            mRequestQueue = new RequestQueue(cache, network);   //Instantiate the RequestQueue with the cache and network. Start the queue.
-            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
-        }
-        return mRequestQueue;
-    }
-
-    public static synchronized MySingleton getmInstance(Context context) {
-        if (mInstance == null) {
-            mInstance = new MySingleton(context);
-        }
-        return mInstance;
-    }
-
-    public <T> void addToRequestQueue(Request<T> request) {
-        mRequestQueue.add(request);
-    }
-}
+//
+//import android.content.Context;
+//
+//import com.android.volley.Cache;
+//import com.android.volley.Network;
+//import com.android.volley.Request;
+//import com.android.volley.RequestQueue;
+//import com.android.volley.toolbox.BasicNetwork;
+//import com.android.volley.toolbox.HurlStack;
+//import com.android.volley.toolbox.Volley;
+//
+//public class MySingleton {
+//    private static MySingleton mInstance;
+//    private RequestQueue mRequestQueue;
+//    private Context mCtx;
+//
+//    public MySingleton(Context mCtx) {
+//        this.mCtx = mCtx;
+//        mRequestQueue = getmRequestQueue();
+//    }
+//
+//    public RequestQueue getmRequestQueue() {
+//        if (mRequestQueue == null) {
+//            Cache cache = new DiskBasedCache(mCtx.getCacheDir(), 1024*1024);     //1MB CAP
+//            Network network = new BasicNetwork(new HurlStack());    //Set up the network to use HttpURLConnection as the HTTP client.
+//            mRequestQueue = new RequestQueue(cache, network);   //Instantiate the RequestQueue with the cache and network. Start the queue.
+//            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+//        }
+//        return mRequestQueue;
+//    }
+//
+//    public static synchronized MySingleton getmInstance(Context context) {
+//        if (mInstance == null) {
+//            mInstance = new MySingleton(context);
+//        }
+//        return mInstance;
+//    }
+//
+//    public <T> void addToRequestQueue(Request<T> request) {
+//        mRequestQueue.add(request);
+//    }
+//}
